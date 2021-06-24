fea593a6a5c085192ff4f056e01feba66d989d7c
KaidenGroves <85904637+Kaiden-Groves@users.noreply.github.com>
2021-06-24 20:22:53 +0000

    version 1.0

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 // Author:
 
 // Global UI Variables
+canvasDiv = createDiv();
+
+canvas = createCanvas(640, 480);
+
+canvas.parent(canvasDiv);
+
+textDiv = createDiv();
+
+textP = createP("Model loading, please wait...");
+
+textP.parent(textDiv);
 //let canvasDiv;
 
 // Global ML Variables
+let mobilenet;
+
+let video;
+
+let isModelReady;
 //let detector;
 
 function setup() {
+  img = loadImage("images/cats-dogs.jpg", imageLoaded);
 
 }
 
@@ -15,20 +32,57 @@ function draw() {
 }
 
 function imageLoaded() {
-
+  detector = ml5.objectDetector("cocossd", modelReady);
 }
 
 function modelReady() {
-
+  detector.detect(canvas, gotResults);
 }
 
 function drawLabel(object) {
+
   // Draw a rectangular outline around the object
 
+  stroke(0, 255, 0);
+
+  noFill();
+
+  rect(object.x, object.y, object.width, object.height);
+
   // Draw the label and its confidence value near the object
+
+  noStroke();
+
+  fill(255, 0, 0);
+
+  textSize(20);
+
+  let label = object.label;
+
+  let confidence = round(object.confidence, 2) * 100;
+
+  text(label + ": " + confidence + "%", object.x + 10, object.y + 20);
 
 }
 
 function gotResults(error, results) {
+
+  if(error) {
+
+    console.error(error);
+
+  } else {
+
+    // console.log(results);
+
+    textP.html("I detect these objects!");
+
+    for(let i = 0; i < results.length; i++) {
+
+      drawLabel(results[i]);
+
+    }
+
+  }
 
 }
