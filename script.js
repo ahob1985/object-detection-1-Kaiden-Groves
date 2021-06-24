594b4af3e0627fcb1be2dfc7402bfb3fbac1e805
KaidenGroves <85904637+Kaiden-Groves@users.noreply.github.com>
2021-06-24 20:32:28 +0000

    Version 1.1

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
 // Author:
 
 // Global UI Variables
-canvasDiv = createDiv();
-
-canvas = createCanvas(640, 480);
-
-canvas.parent(canvasDiv);
-
-textDiv = createDiv();
-
-textP = createP("Model loading, please wait...");
-
-textP.parent(textDiv);
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
 
 // Global ML Variables
-let mobilenet;
-
-let video;
-
-let isModelReady;
-//let detector;
+let detector;
+let img;
 
 function setup() {
+  // Create UI
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP("Model loading, please wait...");
+  textP.parent(textDiv);
+  // Load image 
   img = loadImage("images/cats-dogs.jpg", imageLoaded);
-
 }
 
 function draw() {
@@ -40,49 +35,27 @@ function modelReady() {
 }
 
 function drawLabel(object) {
-
   // Draw a rectangular outline around the object
-
   stroke(0, 255, 0);
-
   noFill();
-
   rect(object.x, object.y, object.width, object.height);
-
   // Draw the label and its confidence value near the object
-
   noStroke();
-
   fill(255, 0, 0);
-
   textSize(20);
-
   let label = object.label;
-
-  let confidence = round(object.confidence, 2) * 100;
-
+  let confidence = floor(object.confidence * 100);
   text(label + ": " + confidence + "%", object.x + 10, object.y + 20);
-
 }
 
 function gotResults(error, results) {
-
   if(error) {
-
     console.error(error);
-
   } else {
-
-    // console.log(results);
-
+    //console.log(results);
     textP.html("I detect these objects!");
-
     for(let i = 0; i < results.length; i++) {
-
       drawLabel(results[i]);
-
     }
-
   }
-
 }
